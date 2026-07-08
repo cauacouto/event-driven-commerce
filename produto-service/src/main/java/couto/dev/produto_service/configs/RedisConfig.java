2bbf92168a9b31c16854c50d935f129adf68ad19
cauacouto <cauacouto022@gmail.com>
2026-07-07 23:55:23 -0300

    Add RedisCacheConfiguration bean for enhanced caching with JSON serialization

--- produto-service/src/main/java/couto/dev/produto_service/configs/RedisConfig.java
+++ produto-service/src/main/java/couto/dev/produto_service/configs/RedisConfig.java
@@ -1,9 +1,29 @@
 package couto.dev.produto_service.configs;
 
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
-}
+
+    @Bean
+    RedisCacheConfiguration redisCacheConfiguration() {
+
+        var serializer = GenericJacksonJsonRedisSerializer.builder()
+                .enableUnsafeDefaultTyping()
+                .build();
+
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair
+                                .fromSerializer(serializer)
+                );
+    }
+
+    }
+

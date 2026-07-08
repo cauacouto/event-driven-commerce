72e303a1bab0b9c5a2dc6cabfbc34751b8c91b66
cauacouto <cauacouto022@gmail.com>
2026-07-07 21:52:27 -0300

    Remove unused method findAllById from ProdutoRepository

--- produto-service/src/main/java/couto/dev/produto_service/repository/ProdutoRepository.java
+++ produto-service/src/main/java/couto/dev/produto_service/repository/ProdutoRepository.java
@@ -3,9 +3,7 @@ package couto.dev.produto_service.repository;
 import couto.dev.produto_service.domin.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
 
-    List<Produto> findAllById(Integer id);
+
 }

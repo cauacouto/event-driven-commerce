0ae0620fc6134023b59ae7a3108faa1efbff9b2d
cauacouto <cauacouto022@gmail.com>
2026-07-07 21:49:10 -0300

    Refactor ProdutoService to use ProdutoResponseDto and add caching for buscarPorId method

---
+++ produto-service/src/main/java/couto/dev/produto_service/configs/RedisConfig.java
@@ -0,0 +1,9 @@
+package couto.dev.produto_service.configs;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+}

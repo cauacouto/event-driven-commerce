261c313deccf53d6346d09fa8755c33d0aeb833f
cauacouto <cauacouto022@gmail.com>
2026-07-07 23:54:24 -0300

    Update EstoqueRepository to use EstoqueResponseDto instead of EstoqueDto

--- estoque-service/src/main/java/couto/dev/estoque_service/database/repository/EstoqueRepository.java
+++ estoque-service/src/main/java/couto/dev/estoque_service/database/repository/EstoqueRepository.java
@@ -1,7 +1,7 @@
 package couto.dev.estoque_service.database.repository;
 
 import couto.dev.estoque_service.database.domin.EstoqueEntity;
-import couto.dev.estoque_service.dto.EstoqueDto;
+import couto.dev.estoque_service.dto.EstoqueResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,5 +10,5 @@ import java.util.Optional;
 public interface EstoqueRepository extends JpaRepository<EstoqueEntity,Integer> {
     Optional<EstoqueEntity> findByProdutoId(Integer produtoId);
 
-    List<EstoqueDto> findAllById(Integer id);
+    List<EstoqueResponseDto> findAllById(Integer id);
 }

4fe98f579632a2ca7c8e03d001ea4c067ce74b17
cauacouto <cauacouto022@gmail.com>
2026-07-07 21:20:19 -0300

    Add EstoqueController with endpoints for managing stock

---
+++ estoque-service/src/main/java/couto/dev/estoque_service/Controller/estoquecontroller.java
@@ -0,0 +1,39 @@
+package couto.dev.estoque_service.Controller;
+
+import couto.dev.estoque_service.Service.EstoqueService;
+import couto.dev.estoque_service.dto.EstoqueDto;
+import couto.dev.estoque_service.dto.EstoqueRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estoque")
+@RequiredArgsConstructor
+public class estoquecontroller {
+
+    private final EstoqueService estoqueService;
+
+    @PostMapping
+    public ResponseEntity<Void> adicionarEstoque(@RequestBody EstoqueDto data){
+        this.estoqueService.AdicionarEstoque(data);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/reserva")
+    public ResponseEntity<Void> ReservaEstoque(@RequestBody EstoqueRequestDto data){
+        this.estoqueService.reservaEstoque(data);
+        return ResponseEntity.ok().build();
+
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<EstoqueDto>> listarEstoque(@PathVariable Integer id){
+        return ResponseEntity.ok(estoqueService.listarPorId(id));
+    }
+
+}

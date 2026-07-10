a6d6472325bfc409c41720135b7c6debcf5e002e
cauacouto <cauacouto022@gmail.com>
2026-07-09 23:36:03 -0300

    feat: adicionar funcionalidade de valor total do produto

--- order-service/src/main/java/couto/dev/order_service/service/OrderService.java
+++ order-service/src/main/java/couto/dev/order_service/service/OrderService.java
@@ -1,27 +1,25 @@
 package couto.dev.order_service.service;
 
+import couto.dev.order_service.Enum.StatusOrder;
 import couto.dev.order_service.domin.OrderEntity;
 import couto.dev.order_service.dto.OrderRequestDto;
 import couto.dev.order_service.dto.ReservaEstoqueDto;
-import couto.dev.order_service.dto.ResponseprodutoDto;
 import couto.dev.order_service.feingClint.estoqueClient;
-import couto.dev.order_service.feingClint.produtoClient;
 import couto.dev.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private  final produtoClient produtoClient;
     private final estoqueClient estoqueClient;
 
 
     public void criarOrder(OrderRequestDto dto){
-        ResponseprodutoDto produto =
-                produtoClient.buscarProduto(dto.getProdutoId());
 
         estoqueClient.reservaEstoque(
                 new ReservaEstoqueDto(dto.getProdutoId(),
@@ -30,8 +28,19 @@ public class OrderService {
         );
 
         OrderEntity order = new OrderEntity();
-        order.setProdutoId(produto.getId());
+        order.setProdutoId(dto.getProdutoId());
         order.setQuantidade(dto.getQuantidade());
+        order.setValorProduto(dto.getValorPorduto());
+        order.setStatusOrder(StatusOrder.AGUARDANDO_PAGAMENTO);
+
+
+        BigDecimal valorTotal = order.getValorProduto()
+                .multiply(BigDecimal.valueOf(dto.getQuantidade()));
+
+        order.setValorTotal(valorTotal);
+
+
+
         orderRepository.save(order);
 
 

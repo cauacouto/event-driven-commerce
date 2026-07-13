2adf2badff8ed29a291d80590c78405c7fe3f9be
cauacouto <cauacouto022@gmail.com>
2026-07-12 22:47:35 -0300

    feat: add RabbitMQ configuration for message handling and exchange setup

---
+++ order-service/src/main/java/couto/dev/order_service/configRabbitmq/RabbitmqConfig.java
@@ -0,0 +1,44 @@
+package couto.dev.order_service.configRabbitmq;
+
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    @Bean
+    public JacksonJsonMessageConverter jacksonJsonMessageConverter(){
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, JacksonJsonMessageConverter converter){
+        RabbitTemplate template =
+                new RabbitTemplate(connectionFactory);
+
+        template.setMessageConverter(converter);
+        return template;
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin (ConnectionFactory connectionFactory){
+        return  new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationEvent> inicializarAdmin(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public DirectExchange orderExchange(){
+        return new DirectExchange("order.ex");
+    }
+}

247298ad9b57649d0226cfde1d56d4bec053258b
cauacouto <cauacouto022@gmail.com>
2026-07-13 00:05:02 -0300

    feat: add mapping for order ID in OrderProducerMapper

--- order-service/src/main/java/couto/dev/order_service/mapping/OrderProducerMapper.java
+++ order-service/src/main/java/couto/dev/order_service/mapping/OrderProducerMapper.java
@@ -3,10 +3,12 @@ package couto.dev.order_service.mapping;
 import couto.dev.order_service.domin.OrderEntity;
 import couto.dev.order_service.dto.OrderProducerDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface OrderProducerMapper {
 
+    @Mapping(source = "order.id",target ="OrderId")
     OrderProducerDto toEvent(OrderEntity order);
 
 }

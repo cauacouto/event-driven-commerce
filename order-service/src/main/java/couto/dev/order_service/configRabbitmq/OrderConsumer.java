f43b8625028b654e4d0811adc0f2a7739e85fcee
cauacouto <cauacouto022@gmail.com>
2026-07-18 23:52:24 -0300

    feat(order): adiciona consumer para pagamento recusado

--- order-service/src/main/java/couto/dev/order_service/configRabbitmq/OrderConsumer.java
+++ order-service/src/main/java/couto/dev/order_service/configRabbitmq/OrderConsumer.java
@@ -1,6 +1,7 @@
 package couto.dev.order_service.configRabbitmq;
 
-import couto.dev.order_service.dto.PagamentoAprovadoEvent;
+import couto.dev.order_service.dto.PagamentoProcessadoEvent;
+import couto.dev.order_service.dto.PagamentoRecusadoDto;
 import couto.dev.order_service.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,9 +16,17 @@ public class OrderConsumer {
 
     private final OrderService orderService;
      @RabbitListener(queues = "pagamentoAprovado.queue")
-    public void ConsumerOrder(@Payload PagamentoAprovadoEvent event){
-         log.info("orderId recebido {}", event.getOrderId());
+    public void Aprovado(@Payload PagamentoProcessadoEvent event){
          orderService.confirmarPagamento(event.getOrderId());
 
      }
+
+
+
+    @RabbitListener(queues = "pagamentoRecusado.queue")
+    public void Recusado(@Payload PagamentoRecusadoDto event){
+        orderService.RecusarPagamento(event.getOrderId());
+
+    }
+
 }

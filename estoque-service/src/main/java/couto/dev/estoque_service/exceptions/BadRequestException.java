b0942d31a43ca4cb548464189b7d939710f0afad
cauacouto <cauacouto022@gmail.com>
2026-07-20 16:36:23 -0300

    feat(estoque): criar execption BadRequestException

---
+++ estoque-service/src/main/java/couto/dev/estoque_service/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package couto.dev.estoque_service.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
